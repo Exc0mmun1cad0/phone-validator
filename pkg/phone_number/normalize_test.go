42f4dca2707efc35c038163ec8fde9194435d797
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-05-29 22:21:20 +0300

    add unit-tests

---
+++ pkg/phone_number/normalize_test.go
@@ -0,0 +1,41 @@
+package phonenumber
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNormalize(t *testing.T) {
+	t.Parallel()
+
+	var tests = []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "Test 1",
+			input: "8(982)583-86-16",
+			want:  "+7-982-583-8616",
+		},
+		{
+			name:  "Test 2",
+			input: "+7 982 583 86-16",
+			want:  "+7-982-583-8616",
+		},
+		{
+			name:  "Test 3",
+			input: "8-986-583-86-16",
+			want:  "+7-986-583-8616",
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, test.want, NormalizePhoneNum(test.input))
+		})
+	}
+}
